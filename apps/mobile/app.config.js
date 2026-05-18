2d44c88607de8ddb9839d6dca7cbb3a907a01ab1
mebishnusahu0595 <mebishnusahu0595@gmail.com>
2026-05-18 20:25:44 +0530

    chore: configure production APK build type and set authorized EAS project ID

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -72,7 +72,7 @@ export default ({ config }) => ({
   extra: {
     apiBaseUrl: process.env.API_BASE_URL,
     eas: {
-      projectId: "228d03d0-6cec-4677-b8a5-f70d3623e394"
+      projectId: "f5e56a43-124c-4fc6-a216-6de1b5c63d4d"
     }
   }
 });
